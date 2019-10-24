4e1f35a634e85c1b81f246967857305ba2fbc6ba
Matthew Statongit config --global user.name Matthew <Malanstaton@gmail.com>
2019-10-24 18:06:33 -0400

    /

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,7 @@ let loginButton = document.querySelector('.loginButton');
 let signUpButton = document.querySelector('.signUpButton');
 let login = document.getElementById('login');
 let signUp = document.getElementById('sign-up');
+let signUpInputs = document.querySelector('.signUpInputs'); 
 
 
 
@@ -12,5 +13,6 @@ login.addEventListener('click', ()=>{
 signUp.addEventListener('click', ()=>{
     signUpButton.classList.add('displayButton');
     loginButton.classList.remove('displayButton');
+    signUpInputs.classList.add('displayButton');
 });
 
