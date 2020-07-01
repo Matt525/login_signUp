5705ae9aaeac469783e0d7fb6b0fce928af04d46
Matthew Staton <malanstaton@gmail.com>
2020-07-01 19:42:15 -0400

    ui update

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,2 @@
-let loginButton = document.querySelector('.loginButton');
-let signUpButton = document.querySelector('.signUpButton');
-let login = document.querySelector("#login");
-let signUp = document.getElementById('sign-up');
-let signUpInputs = document.querySelector('.signUpInputs'); 
-let loginForm = document.getElementById('login_form'); 
-
-
-login.addEventListener('click', ()=>{
-    console.log("login button clicked.");
-});
-
-signUp.addEventListener('click', ()=>{
-    console.log("Sign up button clicked."); 
-    
-});
-
+require('dotenv').config();
+console.log("CONNECTED");
