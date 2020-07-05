e21938679979e16b8f65dd9be20b02ea84e9f8c7
Matthew Staton <malanstaton@gmail.com>
2020-07-04 23:31:37 -0400

    firebase init

---
+++ init-firebase.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBuCdCBVgVnjG7q-b5egL-99-6dasfX0Ms",
+    authDomain: "login-authentication-d517b.firebaseapp.com",
+    databaseURL: "https://login-authentication-d517b.firebaseio.com",
+    projectId: "login-authentication-d517b",
+    storageBucket: "login-authentication-d517b.appspot.com",
+    messagingSenderId: "1051001711710",
+    appId: "1:1051001711710:web:6ec93907431013465f18cf",
+    measurementId: "G-QXV42JJVEL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
